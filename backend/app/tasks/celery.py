0a4c385bc3515b65b3887feb6e6fad6dbc90a23d
Eduardo de Carvalho <eduardofc@ic.ufrj.br>
2026-05-20 12:50:58 -0300

    feat: enable celery as a chronjob

--- backend/app/tasks/celery.py
+++ backend/app/tasks/celery.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.schedules import crontab
 
 from app.core import get_settings
 
@@ -15,12 +16,18 @@ celery.conf.update(
   task_serializer="json",
   accept_content=["json"],
   result_serializer="json",
-  timezone="UTC",
+  timezone="America/Sao_Paulo",
   enable_utc=True,
   task_track_started=True,
   task_acks_late=True,
   worker_prefetch_multiplier=1,
   broker_connection_retry_on_startup=True,
+  beat_schedule={
+    "enforce-retention-policies-daily": {
+      "task": "clean.not_retainable",
+      "schedule": crontab(hour=18, minute=0),
+    },
+  },
 )
 
 celery.autodiscover_tasks(["app.tasks"])
